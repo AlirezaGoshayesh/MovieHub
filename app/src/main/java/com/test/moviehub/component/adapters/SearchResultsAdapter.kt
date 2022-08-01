9caa62375681fc4dae9a740fd9ecc56bad8e01c7
goshayesh <alirezagoshayesh@yahoo.com>
2022-08-01 17:51:02 +0430

    updated gradle things

--- app/src/main/java/com/test/moviehub/component/adapters/SearchResultsAdapter.kt
+++ app/src/main/java/com/test/moviehub/component/adapters/SearchResultsAdapter.kt
@@ -1,13 +1,9 @@
 package com.test.moviehub.component.adapters
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -16,8 +12,6 @@ import com.bumptech.glide.Glide
 import com.test.moviehub.R
 import com.test.moviehub.data.model.MovieResult
 import com.test.moviehub.databinding.ItemMoviewBinding
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.item_moview.view.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
